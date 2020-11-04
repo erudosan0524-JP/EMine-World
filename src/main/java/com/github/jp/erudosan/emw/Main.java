a49a8358ba07f00c0143100c99712ad2d47b77f8
erudo <hashierudo@gmail.com>
2020-11-04 22:33:33 +0900

    [update] add Config Settings in Main class
    MainクラスにConfigの設定を追加

--- src/main/java/com/github/jp/erudosan/emw/Main.java
+++ src/main/java/com/github/jp/erudosan/emw/Main.java
@@ -1,8 +1,15 @@
 package com.github.jp.erudosan.emw;
 
+import com.github.jp.erudosan.emw.utils.Config;
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+
+    private Main instance;
+
+    @Getter
+    private Config myConfig;
 
     @Override
     public void onDisable() {
@@ -11,8 +18,19 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        setInstance(this);
         getLogger().info("プラグインを起動しました");
 
+        myConfig = new Config(getInstance());
+
         getServer().getPluginCommand("emw").setExecutor(new CommandManager());
+    }
+
+    private Main getInstance() {
+        return this.instance;
+    }
+
+    private void setInstance(Main main) {
+        this.instance = main;
     }
 }
