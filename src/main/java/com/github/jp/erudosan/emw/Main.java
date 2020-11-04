74f059573c06ca314b658784a03cd023656ce5b5
erudo <hashierudo@gmail.com>
2020-11-04 21:33:44 +0900

    FIRST COMMIT

---
+++ src/main/java/com/github/jp/erudosan/emw/Main.java
@@ -0,0 +1,16 @@
+package com.github.jp.erudosan.emw;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onDisable() {
+        getLogger().info("プラグインを停止しました");
+    }
+
+    @Override
+    public void onEnable() {
+        getLogger().info("プラグインを起動しました");
+    }
+}
